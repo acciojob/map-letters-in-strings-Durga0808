40faa927c790a34aefaf22320ee5b479b8af2f71
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-26 19:48:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapLetters(input) {
+	 const letterMap = {};
+
+    for (let i = 0; i < input.length; i++) {
+        const currentLetter = input[i];
+
+        // If the letter is not in the map, add it with a new array
+        letterMap[currentLetter] = letterMap[currentLetter] || [];
+        letterMap[currentLetter].push(i);
+    }
+
+    return letterMap; 
+}
+let input=prompt("Enter Input :");
+alert(mapLetters(input));
