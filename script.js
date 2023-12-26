5a4775e409cc2568d693ad73d42954261e3ff8fc
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-26 19:50:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 //your JS code here. If required.
-function mapLetters(input) {
-	 const letterMap = {};
+function mapLetters(word) {
+  const letterMap = {};
 
-    for (let i = 0; i < input.length; i++) {
-        const currentLetter = input[i];
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
 
-        // If the letter is not in the map, add it with a new array
-        letterMap[currentLetter] = letterMap[currentLetter] || [];
-        letterMap[currentLetter].push(i);
+    if (!letterMap.hasOwnProperty(letter)) {
+      letterMap[letter] = [];
     }
 
-    return letterMap; 
+    letterMap[letter].push(i);
+  }
+
+  return letterMap;
 }
-let input=prompt("Enter Input :");
-alert(mapLetters(input));
+
+// Examples
+console.log(mapLetters("dodo"));   // Output: { d: [0, 2], o: [1, 3] }
+console.log(mapLetters("froggy")); // Output: { f: [0], r: [1], o: [2], g: [3, 4], y: [5] }
+console.log(mapLetters("grapes")); // Output: { g: [0], r: [1], a: [2], p: [3], e: [4], s: [5] }
